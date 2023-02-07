580f232f69970c9545c26d2b810c52d525963ccb
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2023-02-07 18:59:02 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-var state = document.getElementById("#state").value;
-var type = document.getElementById("#type").value;
+state = document.getElementById("#state").value;
+type = document.getElementById("#type").value;
 const states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 const types = ["micro", "regional", "brewpub", "large", "planning", "bar", "contract", "proprietor", "closed"]
-
+var stateInput = $('#state').val();
+var typeInput = $('#type').val();
+var state = 'georgia';
 fetch(`https://api.openbrewerydb.org/breweries?by_state=${state}&per_page=50`, {
   method: 'GET',
   credentials: 'same-origin',
@@ -21,6 +23,17 @@ fetch(`https://api.openbrewerydb.org/breweries?by_state=${state}&per_page=50`, {
         return brewery.brewery_type === type;
     })
   console.log(filteredbrew);
-        
-
-});
+  var printBrew = function (brewery) {
+    var breweryEl = $('<li>');
+    var listBrewery = name.street.city.state.postal_code.phone.website_url;
+    listEl.addClass('brewery-list-item').text(listBrewery);
+    listEl.appendTo(breweryListEl);
+  }
+    $("#state").autocomplete({
+    source: states,
+  });
+  $("#type").autocomplete({
+     source: types,
+  });    
+  
+  });
