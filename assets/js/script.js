66b721599c5353aad5952dff4e339dcc7b3e94d5
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2023-02-06 21:05:57 -0500

    added variables and constants to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var state = document.getElementById("#state").value;
 var type = document.getElementById("#type").value;
+const states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+const types = ["micro", "regional", "brewpub", "large", "planning", "bar", "contract", "proprietor", "closed"]
 
 fetch(`https://api.openbrewerydb.org/breweries?by_state=${state}&per_page=50`, {
   method: 'GET',
@@ -16,7 +18,7 @@ fetch(`https://api.openbrewerydb.org/breweries?by_state=${state}&per_page=50`, {
     console.log(data);
     var breweries = data;
     var filteredbrew = breweries.filter(function(brewery) {
-        return brewery.brewery_type === ${type};
+        return brewery.brewery_type === type;
     })
   console.log(filteredbrew);
         
