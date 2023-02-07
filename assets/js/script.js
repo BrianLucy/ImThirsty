57631ca17164bf966b2663d84a4eb9f2b4378674
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2023-02-06 20:29:50 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 fetch('https://api.openbrewerydb.org/breweries', {
   method: 'GET',
   credentials: 'same-origin',
+  per_page: 50,
  })
 
 
  .then(function (response) {
   return response.json();
+  
 })
 .then(function (data) {
-  console.log(data)
+    console.log(data);
+    var breweries = data;
+    var filteredbrew = breweries.filter(function(brewery) {
+        return brewery.brewery_type === "micro";
+    })
+  console.log(filteredbrew);
+        
+
 });
