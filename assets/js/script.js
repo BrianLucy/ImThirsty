e8ffb0138384d3f31dda99f6493e4760b510fecc
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2023-02-06 19:59:52 -0500

    test brewery db api return

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+fetch('https://api.openbrewerydb.org/breweries', {
+  method: 'GET',
+  credentials: 'same-origin',
+ })
 
+
+ .then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data)
+});
